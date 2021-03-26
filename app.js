4dbbc341273e89f91b5e40592d7632ad140fd617
AngelDOrtiz <aortizghs@gmail.com>
2021-03-25 17:50:10 -0700

    nearly finished. added most function

--- app.js
+++ app.js
@@ -1,5 +1,47 @@
 // import functions and grab DOM elements
+import { getRandomThrow } from './utils.js';
+import { doesUserWin } from './utils.js';
 
-// initialize state
+const throwButton = document.getElementById('throw-button');
+
+
+const victories = document.getElementById('victories');
+const losses = document.getElementById('losses');
+const total = document.getElementById('total');
+
+
+    // initialize state
+let lVictories = 0;
+let lTotal = 0; 
+let lDraws = 0; 
+let lLosses = 0;
 
 // set event listeners to update state and DOM
+throwButton.addEventListener('click', () => {
+    const selectedRadioButton = document.querySelector('input:checked');
+    const userChoice = selectedRadioButton.value;
+    
+    const computerResults = getRandomThrow();
+    const userResults = doesUserWin(userChoice, computerResults);
+
+    if (userResults === 'draw') {
+        alert('Draw');
+        lTotal++;
+        lDraws++;
+    }
+
+    if (userResults === 'victory') {
+        alert('Victory');
+        lTotal++;
+        lVictories++;
+    }
+    
+    if (userResults === 'loss') {
+        lTotal++;
+        lLosses++;
+    }
+
+    victories.textContent = `${lVictories}`;
+    losses.textContent = `${lLosses}`;
+    total.textContent = `${lTotal}`;
+});
--- utils.js
+++ utils.js
@@ -1,7 +1,58 @@
-function getRandomThrow(boulder, parchment, shears) {
+export function getRandomThrow() {
 
 //--This should return a random number from 1-3
-    Math.floor(Math.random() * 3) + 1;
+    const rng = Math.floor(Math.random() * 3) + 1;
+    
 
+    if (rng === 1) {
+        return 'boulder';
+    }
 
+    if (rng === 2) {
+        return 'parchment';
+    }
+        
+    
+
+    if (rng === 3) {
+        return 'shears';
+    }
+
+    
 }
+
+export function doesUserWin(player, computer) {
+    
+    if (player === computer) {
+        return 'draw';
+    }
+
+    if (player === 'shears' && computer === 'boulder') {
+        return 'loss';
+    }
+
+    if (player === 'shears' && computer === 'parchment') {
+        return 'victory';
+    }
+
+
+
+    if (player === 'parchment' && computer === 'shears') {
+        return 'loss';
+    }
+
+    if (player === 'parchment' && computer === 'boulder') {
+        return 'victory';
+    }
+
+
+
+    if (player === 'boulder' && computer === 'parchment') {
+        return 'loss';
+    }
+
+    if (player === 'boulder' && computer === 'shears') {
+        return 'victory';
+    }
+}
+console.log(doesUserWin('boulder', 'parchment'));
